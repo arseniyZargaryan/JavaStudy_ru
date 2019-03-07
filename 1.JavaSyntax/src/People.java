06ebae617b60aab99aaf5ddfe39f277a4378543a
ArseniyZargaryan <arsyush@mail.ru>
2019-03-07 20:07:44 +0800

    my first programm

---
+++ 1.JavaSyntax/src/People.java
@@ -0,0 +1,12 @@
+public class People {
+    int age = 5;
+    String name = "wefuyg";
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
