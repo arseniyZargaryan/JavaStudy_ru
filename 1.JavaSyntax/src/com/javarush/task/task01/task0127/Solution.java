2fd9e6864744289b313afa7e3e9c1e0c227dd64e
ArseniyZargaryan <arsyush@mail.ru>
2019-05-19 01:35:11 +0800

    6. работа с методами

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0127/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task01.task0127;
+
+/* 
+Число в квадрате
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = sqr(5);
+        System.out.println(a);
+        //напишите тут ваш код
+    }
+
+    public static int sqr(int a) {
+        int result = a * a;
+        return result;
+    }
+}
