61cd49ce6e611f151dcc7cb47cead1d543b09a6d
ArseniyZargaryan <arsyush@mail.ru>
2019-05-18 22:42:43 +0800

    1. инициализация переменных

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 2567;
+        int b = 23456;
+        String s = "sergoihjerg";
+    }
+}
