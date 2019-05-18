05d090bf37a8e26b38bb84fe863c53ee73232ea4
ArseniyZargaryan <arsyush@mail.ru>
2019-05-19 02:48:02 +0800

    8. умножение , сумма, ссылки

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task02.task0207;
+
+/* 
+Кусочек калькулятора
+*/
+public class Solution {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 150;
+        int y = a + b;
+        int s = a * b;
+        System.out.println(y);
+        System.out.println(s);
+        //напишите тут ваш код
+    }
+}
