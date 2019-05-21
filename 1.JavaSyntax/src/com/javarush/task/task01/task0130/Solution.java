e2b8fa1222d7e36dcdecff0aa4d6fe7bada8378a
ArseniyZargaryan <arsyush@mail.ru>
2019-05-22 02:29:26 +0800

    9. умножение , сумма, конвертер

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0130/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.task0130;
+
+//
+
+//Наш первый конвертер!
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(41));
+    }
+
+    public static double convertCelsiumToFahrenheit(int celsium) {
+
+       double TF = celsium *9/5.0 + 32;
+        //напишите тут ваш код
+
+        return TF;
+    }
+}
